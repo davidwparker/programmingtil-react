41c87d1e4cc2fd06a691430e9ccc4b177cdc53d5
David Parker <davidwparker@gmail.com>
2016-12-01 16:12:11 -0700

    Add screencast 15 - handling user input

---
+++ 0015-user-input/app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import GroceriesComponent from './GroceriesComponent';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GroceriesComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
