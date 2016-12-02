ea9e9d6c0a53a32af61074329a8efeadcc5b5d27
David Parker <davidwparker@gmail.com>
2016-12-01 20:31:35 -0700

    Add screencast 16 - forms

---
+++ 0016-forms/README.md
@@ -0,0 +1,24 @@
+## STARTING OFF:
+
+Make sure you have npm installed.
+Run:
+```
+cd app
+npm install
+npm start
+```
+
+View at: http://localhost:3000/
+
+## CONCEPTS:
+
+Forms
+
+React component that renders a form also controls what happens in that
+form on subsequent user input. An input form element whose value is
+controlled by React in this way is called a "controlled component".
+
+## RESOURCES:
+
+* https://github.com/davidwparker/programmingtil-react/tree/master/0016-forms
+* https://facebook.github.io/react/docs/forms.html
---
+++ 0016-forms/app/src/NameForm.js
@@ -0,0 +1,36 @@
+import React, {
+  Component
+} from 'react';
+
+class NameForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+    alert('A name was submitted: ' + this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default NameForm;
